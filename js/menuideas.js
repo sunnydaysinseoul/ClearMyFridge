6f940e0cfc6039444bf8915f918ba3e0b76984d9
Youngsun Han <youngsunhan.kr@gmail.com>
2023-01-04 20:21:33 +0900

    이미지 절대경로로 변경

--- js/menuideas.js
+++ js/menuideas.js
@@ -1,23 +1,23 @@
 const menus = [
     {
         name : "버거",
-        image : "/img/burger.jpg",
+        image : "../img/burger.jpg",
     },
     {
         name : "볶음면",
-        image : "/img/friednoodles.jpg",
+        image : "../img/friednoodles.jpg",
     },
     {
         name : "팬케익",
-        image : "/img/pancakes.jpg",
+        image : "../img/pancakes.jpg",
     },
     {
         name : "샌드위치",
-        image : "/img/sandwich.jpg",
+        image : "../img/sandwich.jpg",
     },
     {
         name : "요거트볼",
-        image : "/img/yogurtbowl.jpg",
+        image : "../img/yogurtbowl.jpg",
     },
 ];
 
